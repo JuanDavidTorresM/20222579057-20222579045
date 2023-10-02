a61891a653b111329d48fb2a9773497680a3de4c
JuanDavidTorresM <143672405+JuanDavidTorresM@users.noreply.github.com>
2023-10-01 23:07:34 -0500

    barra.py

--- 20222579057-20222579045/frontend/barra/barra.py
+++ 20222579057-20222579045/frontend/barra/barra.py
@@ -1,22 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import html
 
-# Contenido de las pestañas
-tab_inicio_content = html.Div([
-    html.H3("Contenido de la pestaña Inicio"),
-    html.P("Este es el contenido de la pestaña Inicio."),
-])
-
-tab_programa_estudiantes_content = html.Div([
-    html.H3("Contenido de la pestaña Programa Estudiantes"),
-    html.P("Este es el contenido de la pestaña Programa Estudiantes."),
-])
-
-tab_programa_profesional_content = html.Div([
-    html.H3("Contenido de la pestaña Programa Profesional"),
-    html.P("Este es el contenido de la pestaña Programa Profesional."),
-])
-
 # Componente de pestañas
 tabs = dbc.Tabs(
     [
